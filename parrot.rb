911458bd3ce7c6c746f4d982dfe391025e60735d
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-09-01 18:30:17 +1000

    Completed Assignment

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase)
+def parrot(phrase = "Hi, whoever you are")
     puts "#{phrase}"
     return phrase
 end
+
+parrot
 
 parrot("Hi, my name is Vincci")
